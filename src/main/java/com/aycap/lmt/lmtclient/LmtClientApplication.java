b9951ea6dac05f66fa99d46cc806b40170724ac8
unknown <dev.02.easset@gmail.com>
2023-01-09 14:50:39 +0700

    First commit

---
+++ src/main/java/com/aycap/lmt/lmtclient/LmtClientApplication.java
@@ -0,0 +1,13 @@
+package com.aycap.lmt.lmtclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmtClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmtClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aycap/lmt/lmtclient/kafka/ProducerService.java
@@ -0,0 +1,8 @@
+package com.aycap.lmt.lmtclient.kafka;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProducerService {
+
+}
