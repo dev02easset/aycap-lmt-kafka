2efa5f7149b247619f20d9cc318e8fc889f84405
dev02easset <dev.02.easset@gmail.com>
2023-01-09 15:52:23 +0700

    add server

--- src/main/java/com/aycap/lmt/lmtclient/kafka/ProducerService.java
+++ src/main/java/com/aycap/lmt/lmtclient/kafka/ProducerService.java
@@ -1,8 +1,21 @@
 package com.aycap.lmt.lmtclient.kafka;
 
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProducerService {
+    private static final Logger logger = LogManager.getFormatterLogger(Producer.class);
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String msg) {
+        logger.info("Produce Topic: %s - Message: %s", topic, msg);
+        this.kafkaTemplate.send(topic, msg);
+    }
 }
